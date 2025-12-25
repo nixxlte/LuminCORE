d635eb1706ac263f39c738fd748f355ec8da7890
Daniel (Ray) <thebyt3zcode@yahoo.com>
2025-12-25 02:49:22 -0300

    Change LuminSDK run location

--- System/Modifications/NewUser.sh
+++ System/Modifications/NewUser.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 mkdir ~/container # (Yes, thats important =3)
+git clone https://github.com/nixxlte/LuminOS
 sudo apt update && sudo apt upgrade
+cp ~/container/LuminOS/Root/.bashrc ~/.bashrc
+source ~/.bashrc
