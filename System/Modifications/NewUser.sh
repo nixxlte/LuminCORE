3066f76b8c08758d82b4ef43162d2608847be631
Daniel (Ray) <thebyt3zcode@yahoo.com>
2025-12-26 10:23:18 -0300

    Update NewUser.sh

--- System/Modifications/NewUser.sh
+++ System/Modifications/NewUser.sh
@@ -2,6 +2,7 @@
 
 mkdir ~/container # (Yes, thats important =3)
 git clone https://github.com/nixxlte/LuminOS
+cd LuminOS && ls
 sudo apt update && sudo apt upgrade
 cp ~/container/LuminOS/Root/.bashrc ~/.bashrc
 source ~/.bashrc
