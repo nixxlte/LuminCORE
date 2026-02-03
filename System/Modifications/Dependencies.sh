dece1071bfd86c4e293bde919fee2c0d712167d9
Daniel (Ray) <thebyt3zcode@yahoo.com>
2026-02-03 16:51:20 -0300

    New Dependencies.sh

--- System/Modifications/Dependencies.sh
+++ System/Modifications/Dependencies.sh
@@ -1,85 +1,28 @@
-#!/bin/bash
+if [ -n "$BASH_VERSION" ]; then
+    #!/bin/bash
+    echo " "
+fi
 
-sudo apt update && sudo apt upgrade -y
+sudo su
 
-# Hi! this is the script that installs some dependencies, like DE, python, and the most important, dotnet (well, is a C# based OS, what are you expecting?)
-# this script also removes some conflicting packages, like ubuntu-desktop (from the base Ubuntu), it also substutes gnome-terminal with konsole (KDE terminal) and apt with nala
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
 
-# Code by NyanRay64 =3
+  if [ "$ID" = "arch" ]; then
+    echo "Running in Arch-based distro"
+  elif [ "$ID" = "ubuntu" ]; then
+    echo "Running in Ubuntu-based distro"
+  elif [ "$ID" = "debian" ]; then
+    echo "Running in Debian-based distro"
+  elif [ "$ID" = "manhjaro" ]; then
+    echo "Running in Manjaro"
+  else
+    echo "Cant run on unrecognized system: $PRETTY_NAME, if it is based on a supported distro you can run onlineinstaller with -f"
+  fi
+else
+  echo "Cant run on unrecognized system: if it is based on a supported distro you can run onlineinstaller with -f"
+fi
 
-sudo su # this script needs root permissions, cause it installs and sets up somethings
-
-# Install and configure KDE-plasma
-echo "installing and seting up Plasma desktop..."
-apt install kde-plasma -y
-systemctl enable gdm3
-apt remove ubuntu-desktop
-echo "done."
-
-# Set-up new user setting
-mkdir /home/luminos
-cd /home/luminos
-git clone https://github.com/nixxlte/LuminCORE
-cd LuminCORE/System/Modifications
-cp NewUser.sh /home/
-echo "when you create a new user pls run 'sudo /home/NewUser.sh'"
-
-# Install some KDE apps, instead of GNOME apps
-echo "installing KDE applications..."
-apt install kde-applications -y
-apt remove gnome-terminal -y
-apt install nautilus -y
-apt install gnome-sushi -y # i know, is contradictory, but gnome-sushi is a nice file previewer and Ubuntu already needs nautilus to work properly
-apt install tilix # a good terminal emulator :D
-echo "done."
-
-# Install some dependencies
-echo "installing LuminOS dependencies..."
-apt install nala python3 python3-pip git wget neofetch -y
-sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-8.0
-snap install dotnet-runtime --classic
-snap install dotnet --classic
-dotnet new install Avalonia.Templates # Phantom GUI needs this, also almost every UI C# app on Lumin/Linux
-apt install qtchooser
-echo "done."
-
-# Install some Phantom dependencies
-echo "installing Phantom dependencies..."
-sudo apt install \
-    libsdl2-2.0-0 \
-    libsdl2-image-2.0-0 \
-    libsdl2-ttf-2.0-0 \
-    libsdl2-mixer-2.0-0 # Thanks to raice by helping me do the Phantom lib =3
-sudo apt install \
-    libsdl2-2.0-0 \
-    libsdl2-image-2.0-0 \
-    libsdl2-ttf-2.0-0 \
-    libsdl2-mixer-2.0-0
-echo "done."
-
-# Check the instalation
-echo "making sure everything is okay..."
-dotnet --list-sdks
-dotnet --list-runtimes
-git --version
-nala --version
-python3 --version
-echo "done."
-
-# Set up nala
-echo "setting up nala..."
-echo 'alias apt=nala' >> ~/.bashrc
-source ~/.bashrc
-nala update && nala upgrade -y
-echo "done."
-
-# Set up Phantom (build 2097 R2 or newer)
-cd ~/
-git clone https://github.com/nixxlte/Phantom
-cd Phantom
-apt install gcc
-
-# Final message
-neofetch
-echo "You're all set up! welcome to LuminOS!"
+if [ "$1" = "-f" ]; then
+  echo "force installing dependencies (DEBIAN ONLY)"
+fi
