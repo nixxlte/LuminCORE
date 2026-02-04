e9c73391703e9d5adcb8eb57e95b75ea9fef87ae
Daniel (Ray) <thebyt3zcode@yahoo.com>
2026-02-04 15:07:00 -0300

    start making dependencies

--- System/Modifications/Dependencies.sh
+++ System/Modifications/Dependencies.sh
@@ -3,35 +3,55 @@ if [ -n "$BASH_VERSION" ]; then
     echo " "
 fi
 
-sudo su
+debian() {
+  sudo apt update && sudo apt upgrade
+}
 
-if [ -f /etc/os-release ]; then
-  . /etc/os-release
+fedora() {
+  sudo dnv update && sudo dnf upgrade
+}
 
-  if [ "$ID" = "arch" ]; then
-    echo "Running in Arch-based distro"
-  elif [ "$ID" = "ubuntu" ]; then
-    echo "Running in Ubuntu-based distro"
-  elif [ "$ID" = "debian" ]; then
-    echo "Running in Debian-based distro"
-  elif [ "$ID" = "manhjaro" ]; then
-    echo "Running in Manjaro"
-  else
-    echo "Cant run on unrecognized system: $PRETTY_NAME, if it is based on a supported distro you can run onlineinstaller with -f"
-  fi
-else
-  echo "Cant run on unrecognized system: if it is based on a supported distro you can run onlineinstaller with -f"
-fi
+arch() {
+  sudo pacman -Syu
+}
+
+# Code by NyanRay64 =3
 
 if [ "$1" = "-f" ]; then
   # echo "force installing dependencies (DEBIAN ONLY)"
   if [ "$2" = "-deb" ]; then
     echo "force installing dependencies (DEBIAN MODE)"
+    debian
   elif [ "$2" = "-dnf" ]; then
     echo "force installing dependencies (FEDORA MODE)"
+    fedora
   elif [ "$2" = "-pacman" ]; then
     echo "force installing dependencies (ARCH MODE)"
+    arch
   else
     echo "force installing dependencies (DEBIAN DEFAULT)"
+    debian
+  fi
+elif [ "$1" = "" ]; then
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+
+    if [ "$ID" = "arch" ]; then
+      echo "Running in Arch-based distro"
+      arch
+    elif [ "$ID" = "ubuntu" ]; then
+      echo "Running in Ubuntu-based distro"
+      debian
+    elif [ "$ID" = "debian" ]; then
+      echo "Running in Debian-based distro"
+      debian
+    elif [ "$ID" = "manhjaro" ]; then
+      echo "Running in Manjaro"
+      arch
+    else
+      echo "Cant run on unrecognized system: $PRETTY_NAME, if it is based on a supported distro you can run onlineinstaller with -f"
+    fi
+  else
+    echo "Cant run on unrecognized system: if it is based on a supported distro you can run onlineinstaller with -f"
   fi
 fi
